eaff3db7755f17557014cc2ebf9afdcc7130fce2
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2022-06-11 22:12:55 +0200

    allow sending an embed without {embeds:[]}

--- labscore/utils/message.js
+++ labscore/utils/message.js
@@ -2,5 +2,6 @@ module.exports.editOrReply = function(context, message, disableReference = false
   // Apply message_reference
   if(!message.message_reference && !disableReference) message.reference = true
   if(!message.allowedMentions) message.allowedMentions = {parse: [], repliedUser: false}
+  if(!message.content && !message.embed && !message.embeds && !message.components) return context.editOrReply({embeds: [message]})
   return context.editOrReply(message)
 }
