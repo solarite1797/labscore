46c93b0c4e8cf006be9ddcaedf78693e2b5684a3
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2022-06-17 20:18:33 +0200

    nvm i do need express

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { ClusterManager } = require('detritus-client');
+const express = require('express');
+const app = express();
 const time = Date.now();
 
 const token = process.env.token;
 
 let client = `../${__dirname}/labscore/client.js`
-
 if(process.env.environment == "local") client = `./labscore/client.js`
-
 const manager = new ClusterManager(client, token, {
   shardCount: 2,
   shardsPerCluster: 1,
@@ -17,3 +17,11 @@ const manager = new ClusterManager(client, token, {
   await manager.run();
   console.log(`v2 | ready. took ${(Date.now() - time) / 1000}.`)
 })();
+
+app.get("*", function (request, response) {
+  response.send(`🧪 v2 @ ${Date.now()}`);
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log(`v2 | web server live on port ${listener.address().port}.`);
+});
