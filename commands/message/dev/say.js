575bbaa73a060101a238796ed8828df8807ed538
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-04-01 14:00:38 +0200

    speak

---
+++ commands/message/dev/say.js
@@ -0,0 +1,21 @@
+module.exports = {
+  label: "text",
+  name: "say",
+  metadata: {
+    description: 'speak.',
+    description_short: 'speak',
+    examples: ['say hug'],
+    category: 'dev',
+    usage: 'say <text>'
+  },
+  onBefore: context => context.user.isClientOwner,
+  onCancel: ()=>{},
+  run: async (context, args) => {
+    await context.triggerTyping();
+    if(context.message.canDelete) context.message.delete();
+    await context.reply({
+      content: args.text,
+      allowedMentions: {parse: [], repliedUser: false}
+    });
+  }
+};
