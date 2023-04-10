50c0c9a4e5f48abb205a4ad74281888b50d4d192
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-04-10 22:48:15 +0200

    more art

---
+++ commands/message/fun/art2.js
@@ -0,0 +1,72 @@
+//https://resources.jetbrains.com/cai/eidos_231/goland/999.png
+
+const { createEmbed } = require('../../../labscore/utils/embed')
+const { editOrReply } = require('../../../labscore/utils/message')
+
+const APPS = [
+  "appcode",
+  "aqua",
+  "big-data-tools",
+  "clion",
+  "cwm",
+  "datagrip",
+  "datalore",
+  "dataspell",
+  "dotcover",
+  "dotmemory",
+  "dotpeek",
+  "dottrace",
+  "fleet",
+  "gateway",
+  "goland",
+  "grazie",
+  "hub",
+  "intellij-idea",
+  "intellij-idea-community",
+  "intellij-idea-edu",
+  "intellij-rust",
+  "jetbrains",
+  "jb-academy",
+  "kotlin",
+  "license-vault",
+  "marketplace",
+  "mono",
+  "mps",
+  "phpstorm",
+  "pycharm",
+  "pycharm-community",
+  "pycharm-edu",
+  "qa-tools",
+  "qodana",
+  "resharper",
+  "resharper-cpp",
+  "rider",
+  "riderflow",
+  "rubymine",
+  "space",
+  "teamcity",
+  "toolbox",
+  "upsource",
+  "webstorm",
+  "youtrack",
+]
+
+module.exports = {
+  name: 'art2',
+  aliases: ['wallpaper2'],
+  metadata: {
+    description: 'Returns a random colorful wallpaper from JetBrains Eidos.',
+    description_short: 'AI wallpapers',
+    examples: ['art2'],
+    category: 'fun',
+    usage: `art2`
+  },
+  run: async (context) => {
+    context.triggerTyping();
+    return editOrReply(context, createEmbed("default", context, {
+      image: {
+        url: `https://resources.jetbrains.com/cai/eidos_231/${APPS[Math.floor(Math.random()*APPS.length)]}/${Math.floor(1 + Math.random()*999)}.png`
+      }
+    }))
+  }
+};
