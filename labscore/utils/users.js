bb5c16c83bfeb87a84aee24c23aa8547e4ae2698
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-05-29 21:27:17 +0200

    doesnt break anymore if someone has too many badges

--- labscore/utils/users.js
+++ labscore/utils/users.js
@@ -43,62 +43,62 @@ const BADGE_TYPES = Object.freeze({
   "staff": {
     description: "Discord Staff",
     link: "https://discord.com/company",
-    icon: "<:badge_staff:903276633161609246>"
+    icon: "<:b:903276633161609246>"
   },
   "partner": {
     description: "Partnered Server Owner",
     link: "https://discord.com/partners",
-    icon: "<:badge_partner:903276631559389196>"
+    icon: "<:b:903276631559389196>"
   },
   "certified_moderator": {
     description: "Moderator Programs Alumni",
     link: "https://discord.com/safety",
-    icon: "<:badge_mod_new:1049594117849632778>"
+    icon: "<:b:1049594117849632778>"
   },
   "hypesquad": {
     description: "HypeSquad Events",
     link: "https://discord.com/hypesquad",
-    icon: "<:badge_hypesquad:903276631408394351>"
+    icon: "<:b:903276631408394351>"
   },
   "hypesquad_house_1": {
     description: "HypeSquad Bravery",
     link: "https://discord.com/settings/hypesquad-online",
-    icon: "<:badge_hypesquad_bravery:903276631790059540>"
+    icon: "<:b:903276631790059540>"
   },
   "hypesquad_house_2": {
     description: "HypeSquad Brilliance",
     link: "https://discord.com/settings/hypesquad-online",
-    icon: "<:badge_hypesquad_brilliance:903276631261597706>"
+    icon: "<:b:903276631261597706>"
   },
   "hypesquad_house_3": {
     description: "HypeSquad Balance",
     link: "https://discord.com/settings/hypesquad-online",
-    icon: "<:badge_hypesquad_balance:903276631211249674>"
+    icon: "<:b:903276631211249674>"
   },
   "bug_hunter_level_1": {
     description: "Discord Bug Hunter",
     link: "https://support.discord.com/hc/en-us/articles/360046057772-Discord-Bugs",
-    icon: "<:badge_bughunter:903276631173509131>"
+    icon: "<:b:903276631173509131>"
   },
   "bug_hunter_level_2": {
     description: "Discord Bug Hunter",
     link: "https://support.discord.com/hc/en-us/articles/360046057772-Discord-Bugs",
-    icon: "<:badge_bughunter_2:903276883523797033>"
+    icon: "<:b:903276883523797033>"
   },
   "active_developer": {
     description: "Active Developer",
     link: "https://support-dev.discord.com/hc/en-us/articles/10113997751447?ref=badge",
-    icon: "<:active_developer:1112811846009892915>"
+    icon: "<:b:1112811846009892915>"
   },
   "verified_developer": {
     description: "Early Verified Bot Developer",
-    icon: "<:badge_botdev:903276631173509130>",
+    icon: "<:b:903276631173509130>",
     link: "https://discord.com/developers" //not on the actual badge, added for consistency
   },
   "early_supporter": {
     description: "Early Supporter",
     link: "https://discord.com/settings/premium",
-    icon: "<:badge_earlysupporter:903277590956101672>"
+    icon: "<:b:903277590956101672>"
   }
 })
 
@@ -119,7 +119,7 @@ const BADGES = Object.freeze({
 
 function renderBadges(user){
   let badges = [];
-  for(const flag of Object.keys(BADGES)) if(user.hasFlag(parseInt(flag))) badges.push(link(BADGE_TYPES[BADGES[flag]].link, BADGE_TYPES[BADGES[flag]].icon, BADGE_TYPES[BADGES[flag]].description))
+  for(const flag of Object.keys(BADGES)) if(user.hasFlag(parseInt(flag))) badges.push(link(BADGE_TYPES[BADGES[flag]].link, BADGE_TYPES[BADGES[flag]].icon))
   return badges;
 }
 
