eb603ef4c2585928bf1c36bb402d505015afeb6f
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-06-12 13:55:10 +0200

    bring the idiot prefix back

--- labscore/client.js
+++ labscore/client.js
@@ -33,7 +33,7 @@ module.exports.paginator = new Paginator(cluster, {
 
 // Clients
 
-let commandPrefixes = ['lc..','lc.'] // Migration from beta -> main, remove lc.. eventually
+let commandPrefixes = ['lc..', 'ic.', 'lc.'] // Migration from beta -> main, remove lc.. eventually
 if(process.env.PREFIX_OVERRIDE) commandPrefixes = process.env.PREFIX_OVERRIDE.split('|');
 
 const commandClient = new CommandClient(cluster, {
