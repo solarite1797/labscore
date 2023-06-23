6cca320bd74116311f934490c9f3d14db8cae90c
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-06-23 16:00:19 +0200

    add fix to markdown for "empty" codeblocks

--- labscore/utils/markdown.js
+++ labscore/utils/markdown.js
@@ -10,6 +10,7 @@ module.exports.highlight = function(content){
 }
 
 module.exports.codeblock = function(type, content){
+  if(!content.length) return "```" + type + "\n```"
   return "```" + type + "\n" + content.join('\n').replace(/`/g, '`​') + "\n```"
 }
 
