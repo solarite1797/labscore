ed8fa26f2c63aad35dd529b50b124a6420bed5b1
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-07-13 11:55:22 +0200

    potential fix

--- labscore/utils/message.js
+++ labscore/utils/message.js
@@ -1,9 +1,11 @@
+const { Permissions } = require("detritus-client/lib/constants")
+
 module.exports.editOrReply = function(context, message, disableReference = false){
   // Apply message_reference
   if(!message.content && !message.embed && !message.embeds && !message.components && !message.files) message = {embeds: [message]}
   if(!message.message_reference && !disableReference) message.reference = true
   // Disable mentions
   if(!message.allowedMentions) message.allowedMentions = {parse: [], repliedUser: false}
-  // Only respond if the command is still available.
-  if(!context.message.deleted) return context.editOrReply(message)
+  // Only respond if the command is still available and we have permissions to respond.
+  if(!context.message.deleted && context.channel.can(Permissions.SEND_MESSAGES)) return context.editOrReply(message)
 }
