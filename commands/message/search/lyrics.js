99cca895226fe49c20690a20d956a4fa79257ca2
derpystuff <3515180-derpystuff@users.noreply.gitlab.com>
2023-09-30 22:07:44 +0200

    display server provided error messages

--- commands/message/search/lyrics.js
+++ commands/message/search/lyrics.js
@@ -87,11 +87,7 @@ module.exports = {
         pages
       });
     }catch(e){
-      if(e.response?.body?.status){
-        if(e.response.body.status == 2){
-          return editOrReply(context, {embeds:[createEmbed("error", context, `Unable to perform lyrics search.`)]})
-        }
-      }
+      if(e.response?.body?.status && e.response.body.status == 2 && e.response.body.message) return editOrReply(context, {embeds:[createEmbed("error", context, e.response.body.message)]})
       console.log(JSON.stringify(e.raw))
       return editOrReply(context, {embeds:[createEmbed("error", context, `Something went wrong.`)]})
     }
