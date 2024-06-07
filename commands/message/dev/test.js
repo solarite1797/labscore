3c087fd6485c0650301865db07d6a0ffecfdc6f9
bignutty <3515180-bignutty@users.noreply.gitlab.com>
2024-06-08 01:32:16 +0200

    deploy test command

---
+++ commands/message/dev/test.js
@@ -0,0 +1,21 @@
+const { prideborder } = require("#api");
+const { editOrReply } = require("../../../labscore/utils/message");
+
+module.exports = {
+  label: "text",
+  name: "test",
+  metadata: {
+    description: 'test.',
+    description_short: 'test',
+    examples: ['test'],
+    category: 'dev',
+    usage: 'test'
+  },
+  onBefore: context => context.user.isClientOwner,
+  onCancel: ()=>{},
+  run: async (context, args) => {
+    await context.triggerTyping();
+    const a = await prideborder(context, "https://cdn.discordapp.com/emojis/1145727546747535412.png?size=4096")
+    editOrReply(context, "ok");
+  }
+};
