8e9fcc006942d0f096d3c2b3f5f5dfcc25f9c6d0
bignutty <3515180-bignutty@users.noreply.gitlab.com>
2024-07-14 17:18:22 +0200

    log error

--- commands/message/info/appinfo.js
+++ commands/message/info/appinfo.js
@@ -64,6 +64,7 @@ module.exports = {
       application = await superagent.get(`${Routes.URL}/api/v9/applications/${id}/rpc`)
       application = application.body
     }catch(e){
+      console.log(e)
       return editOrReply(context, createEmbed("warning", context, "Invalid Application"))
     }
 
